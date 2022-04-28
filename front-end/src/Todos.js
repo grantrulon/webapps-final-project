55cf7d482633573a11a1f7d59817893bd2bb45b7
Heather White <hlwhite1313@gmail.com>
2022-04-28 16:16:28 -0600

    updating Items.js to be Todos.js

---
+++ front-end/src/Todos.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, useParams} from "react-router-dom";
+function todos() {
+  const [todos, settodos] = useState([]);
+  const {project_id} = useParams();
+  const {_id} = useParams();
+
+  //Analogous to ComponentDidMount; runs on component's mount and first render; runs once
+  useEffect(() => {
+    fetch(`http://localhost:8000/projects/${project_id}/todos`)
+      .then((body) => body.json())
+      .then((json) => settodos(() => [...json]));
+  }, [project_id, _id]);
+
+  return (
+    <>
+      <p>Hello this is project {project_id}'s todos route</p>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todos._id}>
+            <p>{todo.name}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default todos;
