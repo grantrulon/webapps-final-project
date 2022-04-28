d4c4848fe2a24cb6b8244bfbb4759a5a690668c4
Heather White <hlwhite1313@gmail.com>
2022-04-28 16:15:13 -0600

    updating Store.js to be Project.js

---
+++ front-end/src/Project.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+function project() {
+  const [todos, settodos] = useState([]);
+  const {project_id} = useParams();
+  const {todo_id} = useParams();
+
+  //Analogous to ComponentDidMount; runs on component's mount and first render; runs once
+  useEffect(() => {
+    fetch(`http://localhost:8000/projects/${project_id}/todos`)
+      .then((body) => body.json())
+      .then((json) => settodos(() => [...json]));
+  }, [project_id, todo_id]);
+
+
+  return (
+    <>
+      <p>Hello this is project {project_id}'s todos route</p>
+      <ul>
+        {todos.map((todo) => (
+          <a href="/projects/project_id" onclick="location.href=this.href+'/project_id';return false;"><li key={todos.todo_id}>
+            <p>{todo.name}</p>
+          </li> </a>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default project;
